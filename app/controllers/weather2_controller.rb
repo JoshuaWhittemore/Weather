dcf31c3125b4a4c28817f910f4449a473cc941ba
Joshua Whittemore <josh_whittemore@mac.com>
2023-10-26 11:37:46 -0600

    caching is now functional, time to re-read the requirments.

--- app/controllers/weather2_controller.rb
+++ app/controllers/weather2_controller.rb
@@ -10,18 +10,36 @@ class Weather2Controller < ApplicationController
   # index page.
   def retrieve
     puts "Weather2Controller#retrieve is executing"
-    zip = params[:zip]
+    zip = params[:zip].to_s  # defensive coding
 
-    @current_weather = Globals::WeatherClient.current_weather(zip: zip, units: :imperial)
-    @forecast = Globals::WeatherClient.one_call(lat: @current_weather.coord.lat, lon: @current_weather.coord.lon, exclude: ['minutely', 'hourly'], units: :imperial)
+    cached_data = Rails.cache.read(zip) 
 
-    redirect_to weather2_show_path(current_weather: @current_weather, forecast: @forecast.to_json)
+    if cached_data
+      @cache_hit = true
+      @current_weather = cached_data[:current_weather]
+      @forecast = cached_data[:forecast]
+
+    else
+      @cache_hit = false
+
+      cached_data = {}
+      @current_weather = cached_data[:current_weather] = Globals::WeatherClient.current_weather(zip: zip, units: :imperial)
+      @forecast = cached_data[:forecast] = Globals::WeatherClient.one_call(lat: @current_weather.coord.lat, lon: @current_weather.coord.lon, exclude: ['minutely', 'hourly'], units: :imperial)
+
+      Rails.cache.write(zip,  cached_data , expires_in: Globals::WEATHER_TTL)
+    end
+
+#    @current_weather = Globals::WeatherClient.current_weather(zip: zip, units: :imperial)
+#    @forecast = Globals::WeatherClient.one_call(lat: @current_weather.coord.lat, lon: @current_weather.coord.lon, exclude: ['minutely', 'hourly'], units: :imperial)
+
+    redirect_to weather2_show_path(current_weather: @current_weather, forecast: @forecast.to_json, cache_hit: @cache_hit)
   end
 
   def show
     @forecast = JSON.parse(params["forecast"])
     @current_weather = params['current_weather']
-
+    @cache_hit = params['cache_hit']
+    
 #byebug
 
     puts "Weather2Controller#show is executing"
--- config/initializers/globals.rb
+++ config/initializers/globals.rb
@@ -1,8 +1,6 @@
 
 
 module Globals
-  WeatherClient =  OpenWeather::Client.new(
-      #api_key: "9a48e3030902fa5c8f3e224b607be7e1"
-      api_key: ENV['OPEN_WEATHER_API_KEY']
-    )
+  WeatherClient = OpenWeather::Client.new(api_key: ENV['OPEN_WEATHER_API_KEY'])
+  WEATHER_TTL = 1.minute
 end
