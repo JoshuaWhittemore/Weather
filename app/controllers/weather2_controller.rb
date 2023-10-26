1cf4e1154e49c88e3f9e828e39ffc1a38adeb869
Joshua Whittemore <josh_whittemore@mac.com>
2023-10-26 15:20:33 -0600

    ok uses geocoder and now only makes one call to the weather service, also works with location of boise

--- app/controllers/weather2_controller.rb
+++ app/controllers/weather2_controller.rb
@@ -9,39 +9,53 @@ class Weather2Controller < ApplicationController
   # redirect to show if found, else flash the error and redirect to the
   # index page.
   def retrieve
-    puts "Weather2Controller#retrieve is executing"
-    zip = params[:zip].to_s  # defensive coding
+    location_text = params[:location_text]
 
-    cached_data = Rails.cache.read(zip) 
+    # Geocoder gem maintains its own cache which it will use if possible.
+    results = Geocoder.search(location_text)
 
-    if cached_data
-      @cache_hit = true
-      @current_weather = cached_data[:current_weather]
-      @forecast = cached_data[:forecast]
+    # Some zip codes gave me results in other countries.  Filter those out.
+    results.select! { |result| result.country == 'United States' }
 
-    else
-      @cache_hit = false
+    # if no results for US, flash back to index.
+    # Take the first result, which I think will be the most specific.
+    result = results.first
+    weather = Globals::WeatherClient.one_call(lat: result.data['lat'],
+      lon: result.data['lon'], exclude: ['minutely', 'hourly'], 
+      units: :imperial)
+    
+    cached_data = {result: result.data.to_json, weather: weather.to_json}
 
-      cached_data = {}
-      @current_weather = cached_data[:current_weather] = Globals::WeatherClient.current_weather(zip: zip, units: :imperial)
-      @forecast = cached_data[:forecast] = Globals::WeatherClient.one_call(lat: @current_weather.coord.lat, lon: @current_weather.coord.lon, exclude: ['minutely', 'hourly'], units: :imperial)
+    Rails.cache.write(result.place_id,  cached_data , expires_in: Globals::WEATHER_TTL)
 
-      Rails.cache.write(zip,  cached_data , expires_in: Globals::WEATHER_TTL)
-    end
+
+
+#byebug
+#     cached_data = Rails.cache.read(zip) 
+
+#     if cached_data
+#       @cache_hit = true
+#       @current_weather = cached_data[:current_weather]
+#       @forecast = cached_data[:forecast]
+
+#     else
+#       @cache_hit = false
+
+#       cached_data = {}
+#       @current_weather = cached_data[:current_weather] = Globals::WeatherClient.current_weather(zip: zip, units: :imperial)
+#       @forecast = cached_data[:forecast] = Globals::WeatherClient.one_call(lat: @current_weather.coord.lat, lon: @current_weather.coord.lon, exclude: ['minutely', 'hourly'], units: :imperial)
+
+#       Rails.cache.write(zip,  cached_data , expires_in: Globals::WEATHER_TTL)
+#     end
 
 #    @current_weather = Globals::WeatherClient.current_weather(zip: zip, units: :imperial)
 #    @forecast = Globals::WeatherClient.one_call(lat: @current_weather.coord.lat, lon: @current_weather.coord.lon, exclude: ['minutely', 'hourly'], units: :imperial)
 
-    redirect_to weather2_show_path(current_weather: @current_weather, forecast: @forecast.to_json, cache_hit: @cache_hit)
+    redirect_to weather2_show_path(data: cached_data)# current_weather: @current_weather, forecast: @forecast.to_json, cache_hit: @cache_hit)
   end
 
   def show
-    @forecast = JSON.parse(params["forecast"])
-    @current_weather = params['current_weather']
-    @cache_hit = params['cache_hit']
-    
-#byebug
-
-    puts "Weather2Controller#show is executing"
+    @result = JSON.parse(params.dig(:data, :result), symbolize_names: true)
+    @weather = JSON.parse(params.dig(:data, :weather), symbolize_names: true)
   end
 end
