24ac0bc4468dbed930181002efb6b2fba895184e
Joshua Whittemore <josh_whittemore@mac.com>
2023-10-27 08:47:48 -0600

    controller tests work well enough for now

--- test/controllers/weather_controller_test.rb
+++ test/controllers/weather_controller_test.rb
@@ -1,13 +1,27 @@
 require "test_helper"
 
+VCR.configure do |config|
+  config.cassette_library_dir = "test/vcr_cassettes"
+  config.hook_into :webmock
+end
+
 class WeatherControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get weather_index_url
-    assert_response :success
+    VCR.use_cassette(__method__) do
+      get weather_index_url
+      assert_response :success
+    end
   end
 
   test "should get show" do
-    get weather_show_url, params: {location: "Sacramento, CA"}
-    assert_response :success
+    VCR.use_cassette(__method__) do
+      get weather_show_url, params: {location: "Sacramento, CA"}
+      assert_response :success
+
+      assert_equal false, assigns(:weather_cache_hit)
+      assert_equal "Sacramento, Sacramento County, California, United States", 
+        assigns(:result)[:display_name]
+      assert_equal 39.81, assigns(:weather).dig(:current, :temp)
+    end
   end
 end
--- test/services/weather_service_test.rb
+++ test/services/weather_service_test.rb
@@ -1,15 +1,14 @@
 require "test_helper"
 
 VCR.configure do |config|
-  config.cassette_library_dir = 'test/vcr_cassettes'
+  config.cassette_library_dir = "test/vcr_cassettes"
   config.hook_into :webmock
 end
 
 class WeatherServiceTest < ActiveSupport::TestCase
-
   test "gets weather data" do
     VCR.use_cassette(__method__) do
-      geocoder_result = LocationService.find('509 W 32nd Street, Vancouver, WA 98660')
+      geocoder_result = LocationService.find("509 W 32nd Street, Vancouver, WA 98660")
       data = WeatherService.get(geocoder_result)
       weather = JSON.parse(data[:weather], symbolize_names: true)
 
